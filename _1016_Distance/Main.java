a1e40fad880fefa28359a5b31169ed651c188c0b
root <andre_itj@yahoo.com.br>
2016-03-01 14:13:18 -0300

    Read the distance (in km) and calculate how long it takes (in minutes)

---
+++ _1016_Distance/Main.java
@@ -0,0 +1,14 @@
+package _1016_Distance;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int VALUE = scanner.nextInt();
+		
+		System.out.printf("%d minutos\n", VALUE * 2);
+	}
+}
